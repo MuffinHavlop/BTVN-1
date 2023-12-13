334907803e9751eb0e5ec84ec6188882716eacfc
KenPham0604 <134181189+KenPham0604@users.noreply.github.com>
2023-12-13 23:19:20 +0700

    BTVN

---
+++ GiaKiet.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float USD,VND;
+    cout << "Nhap so VND: " << endl;
+    cin >> VND;
+
+    USD = VND / 23000;
+
+    cout << USD;
+    
+    
+    
+    return 0;
+}
